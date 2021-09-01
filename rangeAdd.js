325f330c9d643bb842969496caf94656e36c0bee
Jack Kramer <31866908+jackokramer@users.noreply.github.com>
2021-09-01 11:08:15 -0700

    Delete rangeAdd.js

--- rangeAdd.js
+++
@@ -1,9 +0,0 @@
-let maxCount = function(m,n,ops){
-    let minWidth = m
-    let minHeight = n
-    for(x of ops){
-        minWidth = Math.min(x[0],minWidth)
-        minHight = Math.min(x[1],minHeight)
-    }
-    return minWidth + minHeight
-}
